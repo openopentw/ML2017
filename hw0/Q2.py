152335be2e314845d975778fb24c48ed2121bf04
openopentw <b04902053@ntu.edu.tw>
2017-02-24 10:12:39 +0800

    speed up Q2

--- hw0/Q2.py
+++ hw0/Q2.py
@@ -1,4 +1,3 @@
-#! python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Feb 23 13:47:27 2017
@@ -9,15 +8,14 @@ from scipy.misc import imread
 from scipy.misc import imsave
 
 IN1 = imread(sys.argv[1])
-IN2 = imread(sys.argv[2])
-OUT = IN2
+OUT = imread(sys.argv[2])
 
 INDEX = OUT.shape
 for i in range(INDEX[0]):
     for j in range(INDEX[1]):
         same = 1
         for k in range(INDEX[2]):
-            if IN1[i][j][k] != IN2[i][j][k]:
+            if IN1[i][j][k] != OUT[i][j][k]:
                 same = 0
                 break
         if same == 1:
